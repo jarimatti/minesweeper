d1f19200e638a2f915ff00dea792399884884690
Jarimatti Valkonen <jarimatti@me.com>
2015-05-23 23:08:37 +0300

    Initial supervisor stuff.

--- src/minesweeper_sup.erl
+++ src/minesweeper_sup.erl
@@ -3,13 +3,14 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_child/3]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+%% Helper macro for declaring children of supervisor.
+%% The restart type could be transient instead of temporary.
+-define(CHILD(I, Type), {I, {I, start_link, []}, temporary, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -18,10 +19,18 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+%% Dynamically start a new ms_server child.
+start_child(Width, Height, Mines) ->
+    %% The supervisor name should be {local, ?MODULE}, but I seem to
+    %% have issues with it. Could it be that a firewall rule blocks epmd?
+    %% Because I get a {nodedown,minesweeper_sup} error from that.
+    supervisor:start_child(?MODULE,
+                           [Width, Height, Mines]).
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    {ok, {{simple_one_for_one, 0, 1},
+          [?CHILD(ms_server, worker)]}}.
